bfc30a8c25425e7af6410a5c26a638d8d60f12ef
AbbySantana <96422865+AbbySantana@users.noreply.github.com>
2023-01-24 13:40:23 +0000

    terminado

---
+++ addEventListener/function.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    let button = document.getElementById("pulsa");
+    let count = 0;
+    let maxCount = Math.floor(Math.random() * 11) + 5; // Número aleatorio de veces entre 5 y 15
+    button.addEventListener("mouseover", moveButton);
+    function moveButton() {
+        if (count < maxCount) {
+            count++;
+            let x = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+            let y = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+            button.style.left = x + "px";
+            button.style.top = y + "px";
+        } else {
+            button.removeEventListener("mouseover", moveButton);
+        }
+    }
+    button.addEventListener("click", function () {
+        if (count === maxCount) {
+            if(confirm("¡Me pillaste!")) location.reload();
+        }
+    });
+};
